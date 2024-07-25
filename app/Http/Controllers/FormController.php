1b6e503804cb0b7fe201d50a864ba36dc76b88f0
emelindamendez <emendez@ecomac.cl>
2024-07-25 17:43:45 -0400

    Analisis base de datos

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Form;
+use Illuminate\Http\Request;
+
+class FormController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate(
+            [
+                'name' => 'required|max:140',
+                'system_name' => 'required|in:sac,sentirse en casa,website,postventa',
+                'description' => 'required|max:500',
+                'priority' => 'required|in:baja,media,alta',
+                'file' => 'required|mimes:png,jpg',
+            ]
+        );
+        Form::create($request->only( 'name', 'system_name', 'description', 'priority', 'file'));
+
+        return response()->json(['message' => 'Incidente enviado.'], 201);
+    }
+}
